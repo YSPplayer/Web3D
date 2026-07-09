0263ef18797d6c33ac7f23c2b8cf48fd88563be4
YSPplayer <88619179+YSPplayer@users.noreply.github.com>
2026-07-09 13:34:25 +0800

    更新

---
+++ Chatai/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class ='container'>
+    </div>
+
+</template>
+<script setup>
+
+</script>
+<style scoped>
+.container {
+    background-color: #FFFFFF;
+    /*vw,相对于浏览器窗口的变化尺寸*/
+    width: 100vw; 
+    height: 100vh;
+}    
+</style>
