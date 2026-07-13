78332cd29a9802e93f085e5bb8c42504801b2275
YSPplayer <88619179+YSPplayer@users.noreply.github.com>
2026-07-13 09:31:12 +0800

    chat更新

--- Chatai/src/component/chatcontainer.vue
+++ Chatai/src/component/chatcontainer.vue
@@ -1,12 +1,19 @@
 <template>
     <div class="chatcontainer flex_colum_center">
         <div class="chat_main"></div>
-        <div class="chat_input"></div>
+        <div class="chat_input_container flex_row">
+            <div class="chat_input">
+
+            </div>
+            <img class="chat_post" :src="postChat" :class="fill_img">
+
+            </img>
+        </div>
     </div>
 </template>
 
 <script setup>
-
+ import postChat from "@/assets/post.svg";
 </script>
 
 <style scoped>
@@ -14,15 +21,29 @@
     gap:1rem;
 }
 .chat_main {
+    margin-top: 1rem;
     width: 95%;
-    height:auto;
+    /* 自动占用剩余高度 */
+    flex: 1; 
     border: 1px solid #ccc;
     border-radius: 10px;
 }
 .chat_input {
+    flex: 1;
+    height: 100%;
+}
+.chat_input_container {
      width: 95%;
      height: 7.375rem;
      border-radius: 10px;
-     background-color: aqua;
+     border: 1px solid #ccc;
+     margin-bottom: 1rem;
+}
+.chat_post {
+    margin-top: auto;
+    margin-right: 0.5rem;
+    margin-bottom: 0.5rem;
+    width: 2.5rem;
+    height: 2.5rem;
 }
 </style>
